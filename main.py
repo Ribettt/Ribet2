19d6be4bf57b0cfba7d8618dfb74d5c754fcd1fc
Kepo lu :u <alfatra99@gmail.com>
2020-08-07 04:03:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord, os
+from discord.ext import commands
+
+bot = commands.AutoShardedBot(
+	command_prefix = "ai!", 
+	shard_count = 5
+	)
+
+@bot.event
+async def on_ready():
+	
+	print("Repl.it turn on.")
+	print("{} ready.".format(bot.user.name))
+	
+@bot.event
+async def on_shard_ready(shard_id):
+	
+	print(shard_id)
+	
+bot.run(os.environ.get("Token"))
